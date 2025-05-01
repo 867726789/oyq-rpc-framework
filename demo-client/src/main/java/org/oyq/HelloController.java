5cf5142f182feeba564ce702e36ca75fa9acda1f
欧阳邱 <ouyangqiu2002@163.com>
2025-05-01 11:31:51 +0800

    排除依赖冲突，可使用SpringBoot进行demo测试

--- demo-client/src/main/java/org/oyq/HelloController.java
+++ demo-client/src/main/java/org/oyq/HelloController.java
@@ -2,22 +2,31 @@ package org.oyq;
 
 import org.oyq.annotation.RpcReference;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     @RpcReference(version = "version2", group = "test2")
     private HelloService helloService;
 
-    public void test() throws InterruptedException {
-        String hello = this.helloService.hello(new Hello("111", "222"));
-        System.out.println(hello);
-//        //如需使用 assert 断言，需要在 VM options 添加参数：-ea
-//        assert "Hello description is 222".equals(hello);
-//        Thread.sleep(12000);
-//        for (int i = 0; i < 10; i++) {
-//            System.out.println(helloService.hello(new Hello("111", "222")));
-//        }
+    @RequestMapping("/test")
+    public String test(@RequestBody Hello hello) {
+        return helloService.hello(hello);
     }
+
+//    public void test() throws InterruptedException {
+//        String hello = this.helloService.hello(new Hello("111", "222"));
+//        System.out.println(hello);
+////        //如需使用 assert 断言，需要在 VM options 添加参数：-ea
+////        assert "Hello description is 222".equals(hello);
+////        Thread.sleep(12000);
+////        for (int i = 0; i < 10; i++) {
+////            System.out.println(helloService.hello(new Hello("111", "222")));
+////        }
+//    }
 
 }
--- demo-client/src/main/java/org/oyq/RpcClientMain.java
+++ demo-client/src/main/java/org/oyq/RpcClientMain.java
@@ -1,13 +1,17 @@
 package org.oyq;
 
 import org.oyq.annotation.RpcScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @RpcScan(basePackage = {"org.oyq"})
+@SpringBootApplication
 public class RpcClientMain {
-    public static void main(String[] args) throws InterruptedException {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(RpcClientMain.class);
-        HelloController helloController = (HelloController) applicationContext.getBean("helloController");
-        helloController.test();
+    public static void main(String[] args) {
+        SpringApplication.run(RpcClientMain.class, args);
+//        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(RpcClientMain.class);
+//        HelloController helloController = (HelloController) applicationContext.getBean("helloController");
+//        helloController.test();
     }
 }
